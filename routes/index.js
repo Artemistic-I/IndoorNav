763b5ea05f44b2cb59ea1185c204ff3ab9208b57
Artem <yakovlev.artem77@gmail.com>
2024-08-12 15:54:13 +0100

    Added a view to be optimised for mobile navigation

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'IndoorNav' });
 });
 
+router.get('/nav', function(req, res, next) {
+  res.render('nav', { title: 'IndoorNav' });
+});
+
 module.exports = router;
